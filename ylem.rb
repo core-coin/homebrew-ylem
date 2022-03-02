4e4768667c57f1025fc3cf9afec3091f272f2006
error2215 <slyvkamm@gmail.com>
2022-03-03 00:27:25 +0200

    ylem homebrew

---
+++ ylem.rb
@@ -0,0 +1,21 @@
+class Ylem< Formula
+  desc "Official Ylem implementation"
+  homepage "https://coreblockchain.cc/"
+  head "https://github.com/core-coin/ylem", :using => :git
+  version "0.0.3"
+  sha256 "b1c82f5ffcdf837d900ed02284feca27c5e36dadae1bbaa0c95e091c49ee65c0"
+  license "LGPL-3.0"
+
+  depends_on "cmake"
+  depends_on "git"
+  depends_on "make"
+
+  def install
+    system "mkdir", "build"
+    Dir.chdir('build') do
+        system "cmake", ".."
+        system "make", "-j4"
+        bin.install Dir["solc/solc"]
+    end
+  end
+end
